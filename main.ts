f190ef49fe9f4e0547c00649f9698ffaf496a26d
XiaoWang <1489201050@qq.com>
2023-04-26 10:36:25 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,35 @@
 let shitou = 0
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     shitou = randint(1, 3)
     if (shitou == 1) {
         basic.showLeds(`
             # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        `)
     }
+    
     if (shitou == 2) {
         basic.showLeds(`
             # . . . #
-            . # . # .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
+                        . # . # .
+                        . . # . .
+                        . . # . .
+                        . . # . .
+        `)
     }
+    
     if (shitou == 3) {
         basic.showLeds(`
             # # # # #
-            # . . . #
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
+                        # . . . #
+                        # . . . #
+                        # . . . #
+                        # # # # #
+        `)
     }
+    
 })
